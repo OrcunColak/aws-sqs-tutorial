34bacfceba2d2b070a66d7909c0e1df5c01c2229
Orçun Çolak <orcun.colak@hazelcast.com>
2024-12-06 10:05:27 +0300

    send message example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Docker Terminal
+```
+# awslocal sqs create-queue --queue-name test-queue
+{
+    "QueueUrl": "http://sqs.eu-central-1.localhost.localstack.cloud:4566/000000000000/test-queue"
+}
+```
--- src/main/java/com/colak/SQSTest.java
+++ src/main/java/com/colak/SQSTest.java
@@ -5,6 +5,8 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
 import java.net.URI;
 
@@ -13,9 +15,25 @@ class SQSTest {
 
     private SqsClient sqsClient;
 
+    private static final String localStackEndpoint = "http://localhost:4566";
+
+    // Queue URL for LocalStack
+    private static final String queueUrl = "http://sqs.eu-central-1.localhost.localstack.cloud:4566/000000000000/test-queue";
+
     public static void main() {
-        SQSTest SQSTest = new SQSTest();
-        SQSTest.connect();
+        SQSTest sqsTest = new SQSTest();
+        try {
+            sqsTest.connect();
+            sqsTest.sendMessage();
+        } finally {
+            sqsTest.close();
+        }
+    }
+
+    private void close() {
+        if (sqsClient != null) {
+            sqsClient.close();
+        }
     }
 
     private void connect() {
@@ -25,11 +43,27 @@ class SQSTest {
         sqsClient = SqsClient.builder()
                 // Region doesn't matter for LocalStack
                 .region(Region.EU_CENTRAL_1)
-                .endpointOverride(URI.create("http://localhost:4566"))
+                .endpointOverride(URI.create(localStackEndpoint))
                 .credentialsProvider(credentialsProvider)
                 .build();
         log.info("Connected successfully");
     }
 
+    private void sendMessage() {
+
+        String messageBody = "Hello from SQS!";
+
+        // Send a message
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(messageBody)
+                .delaySeconds(0) // Optional delay in seconds
+                .build();
+
+        SendMessageResponse sendMessageResponse = sqsClient.sendMessage(sendMessageRequest);
+
+        log.info("Message sent with ID: {}", sendMessageResponse.messageId());
+
+    }
 
 }
